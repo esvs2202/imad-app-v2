0033b834d428873fb4b2804615e5790d44ed03ef
esvs2202 <sriramesv96@gmail.com>
2017-03-05 10:27:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,19 +12,7 @@ var pool = new Pool(config);
 });
 app.get('/test-db',function(req,res){
     res.sendFile(path.join(_dirname,'ui','index.html'))});
-pool.query('SELECT * FROM test', function(err,result){
-    if(err){
-        res.status(500).send(err.toString());
-         }
-         else{
-             res.send(JSON.stringify(result));
-         }
-    
-}
-);
 
-//make a select request
-//return a response with results
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
