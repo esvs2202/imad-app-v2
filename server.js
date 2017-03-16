09e1f30308864820e3efe3b74dd02b6105005024
esvs2202 <sriramesv96@gmail.com>
2017-03-16 12:45:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,15 +1,36 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
 var app = express();
+var crypto = require('crypto');
 app.use(morgan('combined'));
+
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 var pool = new Pool(config);
     
 });
+
+function hash (input, salt) {
+//How do we create a hash?
+
+var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+return hashed.toString('hex');
+    
+}
+
+
+app.get('/hash/:input', function(req, res)
+{
+   var hashedString = hash(req,params,input, ' this is some random string');
+req.send(hashedString); 
+}); 
+
+
+
+
 app.get('/test-db',function(req,res){
     res.sendFile(path.join(_dirname,'ui','index.html'))});
 
